2d7f3de989c00f470eb13aa1d2f7102d27b4f5e1
fla <cleanemailaddress420@protonmail.com>
2022-03-15 23:50:07 -0600

    create and test gum contract

---
+++ src/Gum.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Gum is ERC20, Ownable {
+    address public marketplace;
+    address public staking;
+
+    constructor(address _marketplace, address _staking) ERC20("Gum", "GUM") {
+        marketplace = _marketplace;
+        staking = _staking;
+    }
+
+    modifier onlyOwnerOrStaking() {
+        require(msg.sender == owner() || msg.sender == staking);
+        _;
+    }
+
+    function updateMarkeplace(address _marketplace) public onlyOwner {
+        marketplace = _marketplace;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwnerOrStaking {
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Overwrite the transfer function so that GUM can only be sent
+     * to the marketplace.
+     */
+    function transfer(address to, uint256 amount)
+        public
+        override
+        returns (bool)
+    {
+        require(to == marketplace, "can only send GUM to the marketplace");
+        return ERC20.transfer(to, amount);
+    }
+
+    /**
+     * @dev Overwrite the transferFrom function so that GUM can only be
+     * sent to the marketplace.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        require(to == marketplace, "can only send GUM to the marketplace");
+        return ERC20.transferFrom(from, to, amount);
+    }
+}
---
+++ src/test/Gum.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "../Gum.sol";
+
+interface CheatCodes {
+  function prank(address) external;
+}
+
+contract GumTest is DSTest {
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+    Gum gumContract;
+
+    address MARKETPLACE_ADDRESS = address(1);
+    address STAKING_ADDRESS = address(2);
+    address USER_ADDRESS = address(3);
+    address SPENDER_ADDRESS = address(4);
+    address RANDO_ADDRESS = address(5);
+    address NEW_MARKETPLACE_ADDRESS = address(6);
+
+    function setUp() public {
+        gumContract = new Gum(MARKETPLACE_ADDRESS, STAKING_ADDRESS);
+        gumContract.mint(USER_ADDRESS, 1_000_000);
+    }
+
+    function testMarketplaceTransfer() public {
+        cheats.prank(USER_ADDRESS);
+        bool success = gumContract.transfer(MARKETPLACE_ADDRESS, 100);
+        assertTrue(success);
+    }
+
+    function testFailTransfer() public {
+        cheats.prank(USER_ADDRESS);
+        bool success = gumContract.transfer(RANDO_ADDRESS, 100);
+        assertTrue(success);
+    }
+
+    function testOwnerMint() public {
+        gumContract.mint(USER_ADDRESS, 100);
+    }
+
+    function testStakingMint() public {
+        cheats.prank(STAKING_ADDRESS);
+        gumContract.mint(USER_ADDRESS, 100);
+    }
+
+    function testFailMint() public {
+        cheats.prank(RANDO_ADDRESS);
+        gumContract.mint(USER_ADDRESS, 100);
+    }
+
+    function testUpdateMarketplace() public {
+        address oldMarketplace = gumContract.marketplace();
+        assertEq(oldMarketplace, MARKETPLACE_ADDRESS);
+        gumContract.updateMarkeplace(NEW_MARKETPLACE_ADDRESS);
+        address newMarketplace = gumContract.marketplace();
+        assertEq(newMarketplace, NEW_MARKETPLACE_ADDRESS);
+    }
+
+    function testFailUpdateMarketplace() public {
+        cheats.prank(RANDO_ADDRESS);
+        gumContract.updateMarkeplace(NEW_MARKETPLACE_ADDRESS);
+    }
+
+    function testMarketplaceTransferFrom() public {
+        cheats.prank(USER_ADDRESS);
+        gumContract.approve(SPENDER_ADDRESS, 100);
+        cheats.prank(SPENDER_ADDRESS);
+        gumContract.transferFrom(USER_ADDRESS, MARKETPLACE_ADDRESS, 100);
+    }
+
+    function testFailTransferFrom() public {
+        cheats.prank(USER_ADDRESS);
+        gumContract.approve(SPENDER_ADDRESS, 100);
+        cheats.prank(SPENDER_ADDRESS);
+        gumContract.transferFrom(USER_ADDRESS, RANDO_ADDRESS, 100);
+    }
+}
