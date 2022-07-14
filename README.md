8472675626447bd84cf9357f0cba5715355038a0
fla <101691949+flaflafla@users.noreply.github.com>
2022-07-14 01:27:40 -0400

    emit withdrawn event (#4)
    * redeployed contract
    
    * emit withdrawn event

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ deployed/verified on kovan testnet at [0xe5f1433b6eCc6bE74E413b54f4c1eA2671b1cA0
 
 `src/Staking.sol`
 
-deployed/verified on kovan testnet at [0x6eC471fC2D55Db45dE540201170CD2d7D3A95ed3](https://kovan.etherscan.io/address/0x6ec471fc2d55db45de540201170cd2d7d3a95ed3#code)
+deployed/verified on kovan testnet at [0xC77Aa4F7c4aeb709F045edC8fab54F4a6595f61E](https://kovan.etherscan.io/address/0xc77aa4f7c4aeb709f045edc8fab54f4a6595f61e#code)
---
+++ scripts/testnetTesting.js
@@ -0,0 +1,48 @@
+const { ethers } = require("ethers");
+const kidsAbi = require("./abis/kids.json");
+const pupsAbi = require("./abis/pups.json");
+const stakingAbi = require("./abis/staking.json");
+const gumAbi = require("./abis/gum.json");
+
+const { GUM_PRIV_KEY, GUM_ADDY, GUM_RANDO_PRIV_KEY, GUM_RANDO_ADDY, RPC_URL } =
+  process.env;
+
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+const signer = new ethers.Wallet(GUM_PRIV_KEY, provider);
+
+const kidsAddress = "0x13a5f9a34a5597ac821f551447446d363a945569";
+const pupsAddress = "0xf0a2f967a55492b3db4ffbf1ee7de6bd5cf34009";
+const stakingAddress = "0xC77Aa4F7c4aeb709F045edC8fab54F4a6595f61E";
+const gumAddress = "0xe5f1433b6eCc6bE74E413b54f4c1eA2671b1cA0F";
+const randomAddress = "0x03801efb0efe2a25ede5dd3a003ae880c0292e4d";
+
+const kidsContract = new ethers.Contract(kidsAddress, kidsAbi, signer);
+const pupsContract = new ethers.Contract(pupsAddress, pupsAbi, signer);
+const stakingContract = new ethers.Contract(stakingAddress, stakingAbi, signer);
+const gumContract = new ethers.Contract(gumAddress, gumAbi, signer);
+
+const go = async () => {
+  // const tx = await gumContract.updateStaking(stakingAddress);
+  // const tx = await kidsContract.mint(1, {
+  //   value: ethers.utils.parseEther("0.06"),
+  // });
+  // const tx = await pupsContract.purchasePuppies(1, {
+  //   value: ethers.utils.parseEther("0.06"),
+  // });
+  // const owner = await kidsContract.ownerOf(4);
+  // console.log({ owner });
+  // const tx = await stakingContract.start();
+  // const tx = await kidsContract.approve(stakingAddress, 5);
+  // const tx = await stakingContract.deposit([5], [0]);
+  // const tx = await stakingContract.withdraw([5], [0]);
+  // const tx = await stakingContract.claimRewards();
+  // const lockBlock = await stakingContract.lockBlocks(0, 4);
+  // console.log({ lockBlock });
+  // const tx = await stakingContract.lock([4], [3], [0]);
+  // const tx = await pupsContract.approve(stakingAddress, 4);
+  // const tx = await kidsContract.transferFrom(GUM_ADDY, GUM_RANDO_ADDY, 4);
+  // const res = await tx.wait();
+  // console.log({ tx, res });
+};
+
+go();
